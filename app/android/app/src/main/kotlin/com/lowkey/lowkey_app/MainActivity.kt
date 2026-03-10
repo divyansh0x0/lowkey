acb5a093f6239450ac87a13597293d596e9a97c2
ayush00git <ayush.2007.iit@gmail.com>
2026-03-11 01:07:16 +0530

    fix: black screen on screenshots

--- app/android/app/src/main/kotlin/com/lowkey/lowkey_app/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/lowkey/lowkey_app/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.lowkey.lowkey_app
 
 import io.flutter.embedding.android.FlutterActivity
+import android.view.WindowManager
+import android.os.Bundle
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
